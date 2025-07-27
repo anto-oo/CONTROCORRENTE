9b8d910429822fe05381c6eb5b13c3bd26f43e54
Antonio Fusco <antonio.fusco15@icloud.com>
2025-07-27 11:04:43 +0200

    added files

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const AUTH_USERS: record<string, string> = {
+    'admin':'admin',
+    'peppe':'seppepepe08',
+    'irene':'stairway650',
+}
+
+export function middleware(request: NextRequest) {
+    const authHeader = request.headers.get('Authorization');
+    if (!authHeader) {
+        return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const [username, password] = atob(authHeader.split(' ')[1]).split(':');
+    if (AUTH_USERS[username] !== password) {
+        return new NextResponse('Forbidden', { status: 403 });
+    }
+
+export function middleware(request: NextRequest) {
+    const authHeader = request.headers.get('Authorization');
+    if (!authHeader) {
+        return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const [username, password] = atob(authHeader.split(' ')[1]).split(':');
+    if (AUTH_USERS[username] !== password) {
+        return new NextResponse('Forbidden', { status: 403 });
+    }
+
+    return NextResponse.next();}}
